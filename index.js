cfe5615e1a53dfbf238519fb09cdda04b41261bf
Piso Mojado Grande <becky_weiss@hotmail.com>
2016-01-01 15:17:56 -0500

    Have the response return the container id

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const http = require('http');
+const exec = require('child_process');
 
 const port = 3000;
 
+const getMyContainerIdCommand = 'cat /proc/self/cgroup | sed \'s/^.*docker\///\' | tail -n 1'
+const containerId = exec.execSync(getMyContainerIdCommand, {encoding: 'utf8'}) || 'unknown';
+console.log("Container id " + containerId);
+
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.end("Hello from container " + containerId);
 }).listen(port, function() {
   //console.log(JSON.stringify(process.env, null, 2));
   console.log("Server running on port " + port);
