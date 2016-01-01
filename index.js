9f0eb98d61b5b5b55603208b99e5f9e9ad524af5
Piso Mojado Grande <becky_weiss@hotmail.com>
2015-12-31 22:11:14 -0500

    Hello World nodejs server

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port, hostname, function() {
+  //console.log(JSON.stringify(process.env, null, 2));
+  console.log("Server running at http://${hostname}:${port}/");
+});
