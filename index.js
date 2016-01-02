cef94e8790a4bd3bff1f446b743a556624132215
Piso Mojado Grande <becky_weiss@hotmail.com>
2016-01-02 15:44:53 -0500

    Use express

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const exec = require('child_process');
 
 const port = 3000;
@@ -6,12 +7,17 @@ const port = 3000;
 const getMyContainerIdCommand = 'cat /proc/self/cgroup | grep docker | tail -n 1'
 const commandOutput = exec.execSync(getMyContainerIdCommand, {encoding: 'utf8'}) || "1:blkio:/docker/UNKNOWN\n";
 const containerId = commandOutput.replace(/^.*docker\//, "").trim();
-console.log("Container id " + containerId);
 
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end("Hello from container " + containerId);
-}).listen(port, function() {
-  //console.log(JSON.stringify(process.env, null, 2));
-  console.log("Server running on port " + port);
+app.containerId = containerId;
+
+app.get('/', function(req, res) {
+  res.send("Hello from container " + containerId);
 });
+
+var server = app.listen(port, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
